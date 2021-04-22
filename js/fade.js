2ef7869459dddfea65fc654f115288701c76935d
taeri99 <82636109+taeri99@users.noreply.github.com>
2021-04-23 06:28:56 +0900

    Add files via upload

---
+++ js/fade.js
@@ -0,0 +1,12 @@
+jQuery(function($) {
+    $("body").css("display", "none");
+    $("body").fadeIn(1500);
+    $("a.transition").click(function(event){
+        event.preventDefault();
+        linkLocation = this.href;
+        $("body").fadeOut(1000, redirectPage);
+    });
+    function redirectPage() {
+    window.location = linkLocation;
+    }
+});
